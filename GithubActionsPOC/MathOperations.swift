9ab644a293105bba49ee7bec665194b8cb85676e
KarthickR511 <102885918+KarthickR511@users.noreply.github.com>
2022-04-03 01:14:28 -0400

    Initial commit

---
+++ GithubActionsPOC/MathOperations.swift
@@ -0,0 +1,20 @@
+//
+//  MathOperations.swift
+//  GithubActionsPOC
+//
+//  Created by Karthick R on 4/2/22.
+//
+
+import Foundation
+
+class MathOperations {
+    init() { }
+    
+    func addition(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+    func subtraction(num1: Int, num2: Int) -> Int {
+        return num1 - num2
+    }
+}
